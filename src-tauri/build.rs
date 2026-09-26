3a5d4ae6515f16a97ad03acbf727d14b56c9fae2
fraa2a <123754424+fraa2a@users.noreply.github.com>
2026-09-26 20:02:40 +0200

    fix: use canonical Cargo test link arguments

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -5,10 +5,10 @@ fn main() {
 
     if std::env::var("CARGO_CFG_TARGET_OS").as_deref() == Ok("windows") {
         let manifest = Path::new(env!("CARGO_MANIFEST_DIR")).join("windows-test.manifest");
-        println!("cargo:rerun-if-changed={}", manifest.display());
-        println!("cargo:rustc-link-arg-tests=/MANIFEST:EMBED");
+        println!("cargo::rerun-if-changed={}", manifest.display());
+        println!("cargo::rustc-link-arg-tests=/MANIFEST:EMBED");
         println!(
-            "cargo:rustc-link-arg-tests=/MANIFESTINPUT:{}",
+            "cargo::rustc-link-arg-tests=/MANIFESTINPUT:{}",
             manifest.display()
         );
     }
